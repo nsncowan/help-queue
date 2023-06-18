49b50641525a7ed242982b3e9b943ae6f488a4c8
Noah Cowan <nsncowan@gmail.com>
2023-06-18 11:29:39 -0700

    Using a Timer to Demonstrate Lifecycle Methods

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -21,6 +21,29 @@ class TicketControl extends React.Component {
     };
   }
 
+  // Add the following code after the constructor.
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    1000
+    );
+  }
+
+  // We won't be using this method for our Help Queue update — but it's important to see how it works.
+  componentDidUpdate() {
+    console.log("component updated!");
+  }
+
+  componentWillUnmount(){
+    console.log("component unmounted!");
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime = () => {
+    console.log("tick");
+  }
+
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
