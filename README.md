f83deb70e07991ef9b269203bb52d8707a61be28
Noah Cowan <nsncowan@gmail.com>
2023-06-18 11:56:45 -0700

    save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://github.com/epicodus-lessons/react-with-redux-completed-help-queue/tree/main
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,40 +2,42 @@ import rootReducer from '../../reducers/index';
 import { legacy_createStore as createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import ticketListReducer from '../../reducers/ticket-list-reducer';
+import * as c from './../../actions/ActionTypes';
 
 let store = createStore(rootReducer);
+
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      mainTicketList: {},
+      masterTicketList: {},
       formVisibleOnPage: false
     });
   });
 
   test('Check that initial state of ticketListReducer matches root reducer', () => {
-    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
   });
   
-  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+  test('Check that initial states of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
-  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+  test('Check that initial state of ticketListReducer matches root reducer', () => {
     const action = {
-      type: 'ADD_TICKET',
+      type: c.ADD_TICKET,
       names: 'Ryan & Aimen',
       location: '4b',
       issue: 'Redux action is not working correctly.',
       id: 1
     }
     store.dispatch(action);
-    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
   });
   
-  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
     const action = {
-      type: 'TOGGLE_FORM'
+      type: c.TOGGLE_FORM
     }
     store.dispatch(action);
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
